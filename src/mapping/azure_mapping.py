7d3d582922f6304cf4a832abfb17a59cc9ac9401
030938 <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>
2026-08-05 04:33:30 -0300

    Adicionei o mapping da Azure + converter com as colunas que precisam de tratamento

---
+++ src/mapping/azure_mapping.py
@@ -0,0 +1,19 @@
+AZURE_MAPPING = {
+    # Descrição e Período de Cobrança
+    "ProductName": "ChargeDescription",
+    "Date": "ChargePeriodStart",
+    "BillingPeriodStartDate": "BillingPeriodStart",
+
+    # Contas
+    "BillingProfileId": "BillingAccountId",
+    "BillingProfileName": "BillingAccountName",
+
+    # Moeda
+    "BillingCurrency": "BillingCurrency",
+
+    # Serviços e Recursos
+    "PublisherName": "PublisherName",
+
+    # Custos
+    "PaygCostInBillingCurrency": "ListCost",
+}
